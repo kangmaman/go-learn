d8a66a5aeb1c92ed29e8d82c3d5a1f60e5799ddd
Maman Tirta Rukmana <kangmaman@gmail.com>
2020-02-12 07:34:48 +0700

    new

---
+++ main/app.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kangmaman/numanip/calc"
+)
+
+func main() {
+	result := calc.Add(1, 2)
+	fmt.Printf("result is %d\n", result)
+}
---
+++ main/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.13
+
+require github.com/kangmaman/numanip v1.0.5
